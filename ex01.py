719945a7b804ccd8c7459e06784301d3140626b7
MFTECH-code <RM86782@fiap.com.br>
2021-04-05 08:15:55 -0300

    Correção exercício 1

--- ex01.py
+++ ex01.py
@@ -1,9 +1,15 @@
 salario = float(input("Digite o valor do seu salário bruto: R$"))
-emprestimo = float(input("Digite o valor do empréstimo: R$"))
-
-# O valor do empréstimo não pode passar de 30% do valor do salário 
-
-if salario * 0.3 >= emprestimo:
-    print("Empréstimo aceito com sucesso!")
+if (salario > 0):
+    limite = salario * 0.3
+    print(f"Limite da prestação: R${limite}")
+    emprestimo = float(input("Digite o valor do empréstimo: R$"))
+    if (emprestimo > 0):
+        # O valor do empréstimo não pode passar de 30% do valor do salário 
+        if salario * 0.3 >= emprestimo:
+            print("Empréstimo aceito com sucesso!")
+        else:
+            print("Empréstimo negado! Salário incompatível...")
+    else:
+        print("Valor da prestação inválido...")
 else:
-    print("Empréstimo negado! Salário incompatível...")
+    print("Valor do salário inválido")
